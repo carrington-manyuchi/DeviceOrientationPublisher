88a4c4f1e37bc0b3dd760f7417cb52efdb4b8ff9
Carrington Manyuchi <carrington.manyuchi@standardbank.co.za>
2025-05-19 06:46:28 +0200

    Initial Commit

---
+++ DeviceOrientationPublisher/DeviceOrientationPublisherApp.swift
@@ -0,0 +1,17 @@
+//
+//  DeviceOrientationPublisherApp.swift
+//  DeviceOrientationPublisher
+//
+//  Created by Manyuchi, Carrington C on 2025/05/19.
+//
+
+import SwiftUI
+
+@main
+struct DeviceOrientationPublisherApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
